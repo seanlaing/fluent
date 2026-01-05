083e6c09b62bceeb0435e3d64c5d150931a886a1
Sean Laing <seanlaing@Seans-MacBook-Pro.local>
2026-01-05 14:09:03 -0800

    Playing with sorting

--- FrenchDeck.py
+++ FrenchDeck.py
@@ -34,5 +34,18 @@ if __name__ == '__main__':
 
     # print("Every 13th card:")
     # print(deck[12::13])
-    for card in deck:
+    # for card in deck:
+    #     print(card)
+
+    print(Card('Q', 'spades') in deck)
+
+    print(Card('7', 'beast') in deck)
+
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit] 
+
+    for card in sorted(deck, key=spades_high):
         print(card)
