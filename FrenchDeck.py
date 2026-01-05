293c7d80196ea558635860ed2391ea500408f963
Sean Laing <seanlaing@Seans-MacBook-Pro.local>
2026-01-05 13:11:15 -0800

    Add FrenchDeck example

---
+++ FrenchDeck.py
@@ -0,0 +1,38 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+    
+if __name__ == '__main__':
+    beer_card = Card('7', 'diamonds')
+    print(beer_card)
+    deck = FrenchDeck()
+    # print(len(deck))
+    # print(deck[0])
+    # print(deck[-1])
+    # print("Random cards:")
+    # from random import choice
+    # print(choice(deck))
+    # print(choice(deck))
+    # print(choice(deck))
+
+    # print("First three cards:")
+    # print(deck[:3])
+
+    # print("Every 13th card:")
+    # print(deck[12::13])
+    for card in deck:
+        print(card)
